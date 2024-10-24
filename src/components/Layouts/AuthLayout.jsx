d6f96523479203ab4cb5224f7a23b419a7f48625
BagjaSatrio <muhamadbagjasatrio@gmail.com>
2024-10-25 01:02:07 +0700

    update

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from "../Elements/Logo";
 import FormSignIn from "../Fragments/FormSignIn";
 
@@ -94,10 +95,14 @@ const AuthLayout = (props) => {
       <span className="text-sm text-gray-03">
         Already have an account?&nbsp;
       </span>
-      <a className="text-primary text-sm font-bold">Sign In Here</a>
+      <Link to="/login" className="text-primary text-sm font-bold">
+      Sign In Here
+      </Link>
     </>
   ) : (
-    <a className="text-primary text-sm font-bold">Create an account</a>
+    <Link to="/register" className="text-primary text-sm font-bold">
+      Create an account
+      </Link>
   )}
 </div>
 {/* link end */}
