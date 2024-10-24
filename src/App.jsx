01666000ea05c5b3ab7e487575e4c240cfa3cf5e
BagjaSatrio <muhamadbagjasatrio@gmail.com>
2024-10-25 00:56:25 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./Pages/signIn";
+import SignUpPage from "./Pages/signUp";
+import ErrorRoute from "./Pages/errorRoute";
 
 const App = () => {
-	return (
-<>
-<SignInPage />
-</>
-	);
-  };
-  
-  export default App;
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Halaman Utama</div>,
+      errorElement: <ErrorRoute />
+    },
+    {
+      path: "/login",
+      element: <SignInPage />,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+};
+
+export default App;
